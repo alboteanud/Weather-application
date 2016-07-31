a755b3605b45823dbc5666b22bfae70fb3428bd9
Dan <alboteanud@gmail.com>
2016-07-31 16:18:24 +0300

    c

--- app/src/main/java/com/alboteanu/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/alboteanu/android/sunshine/app/data/WeatherContract.java
@@ -32,7 +32,7 @@ public class WeatherContract {
     // device.
 
 
-    public static final String CONTENT_AUTHORITY = "com.alboteanu.sunshine.StPetersburg";
+    public static final String CONTENT_AUTHORITY = "com.alboteanu.sunshine.Irvine";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
     // the content provider.
